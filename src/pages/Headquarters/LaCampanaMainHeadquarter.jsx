28be3b89f9ce682b6d8019175fb9f00c35898670
jonathantombe <jhontombe33@gmail.com>
2025-05-07 16:08:55 -0500

    Se completó la implementación de Programas Académicos. J.T.Y

--- src/pages/Headquarters/LaCampanaMainHeadquarter.jsx
+++ src/pages/Headquarters/LaCampanaMainHeadquarter.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import HeadquarterImageCarousel from "../../components/common/HeadquarterImageCarousel/HeadquarterImageCarousel"
 import { laCampanaImages } from "../../data/LaCampanaCarouselData"; 
+import AcademicPrograms from "../../components/modules/academic/AcademicPrograms/AcademicPrograms"
+
 
 const LaCampanaMainHeadquarter = () => {
   return (
     <div>
       <main>
-        <HeadquarterImageCarousel
-          slides={laCampanaImages}
-        />
+        <HeadquarterImageCarousel slides={laCampanaImages} />
+        <AcademicPrograms />
       </main>
     </div>
   );
