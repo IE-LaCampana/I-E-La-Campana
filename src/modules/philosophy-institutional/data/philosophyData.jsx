9badd5e0d09a313f4f25b2152503003cc8a85ab7
jonathantombe <jhontombe33@gmail.com>
2025-07-04 10:37:17 -0500

    Se completó la implementación completa de la página de Filosofía Institucional

---
+++ src/modules/philosophy-institutional/data/philosophyData.jsx
@@ -0,0 +1,9 @@
+import FilosofiaInstitucional from "../../../assets/institutional/filosofia-institucional.jpeg"
+
+export const philosophyContent = {
+  title: "FILOSOFÍA INSTITUCIONAL",
+  description:
+    "La Institución Educativa La Campana, ubicada en el departamento del Cauca y profundamente enraizada en los valores de la comunidad Misak, orienta su labor educativa hacia la formación integral de seres humanos con sentido crítico, identidad cultural y compromiso social. Nuestra filosofía promueve el respeto por la vida, la tierra y la sabiduría ancestral, integrando el conocimiento propio con los saberes universales. A través de un enfoque intercultural, se fortalecen procesos pedagógicos que dignifican la cultura Misak, contribuyendo al desarrollo autónomo, armónico y sostenible de la comunidad.",
+  image: FilosofiaInstitucional,
+  alt: "Personas reunidas en comunidad educativa",
+};
